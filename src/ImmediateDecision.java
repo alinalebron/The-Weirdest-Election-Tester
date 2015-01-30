0b9f952e00904a0b1d973c163f955347a707a4aa
ALINA LEBRON <LEBRON.ALI23@GMAIL.COM>
2015-01-30 00:13:28 -0500

    Added instance variables

--- src/ImmediateDecision.java
+++ src/ImmediateDecision.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 /**
+ * A class that represents the Immediate Decision process:
+ * it selects a winner by repeatedly selecting the candidate
+ * with the fewest votes and removing them from the election
+ * (by using the candidatesWithFewest method from the VotersBallot
+ * class) until only one candidate remains
+ *
  * Created by alina on 1/27/15.
  */
 public class ImmediateDecision {
+
+    private ArrayList<String> candidateList; // represents one candidate list
+    private ArrayList<VoterBallots> election; // represents one set of voter ballots
+
+
 
 
 }
